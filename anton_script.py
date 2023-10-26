097082f8775c8b236d0813f3dbbba684a866c6b1
Ole Rößler <ole.nelson.r@gmail.com>
2023-10-26 14:15:22 +0200

    implemented basic strips without error handling

--- anton_script.py
+++ anton_script.py
@@ -1,5 +1,92 @@
 from helper import Interface
+from dotenv import dotenv_values
+import requests
+import json
+
+secrets = dotenv_values(".env")
+api_key = secrets["MOISES_API_KEY"]
 
 
-Interface()
 
+def test_api_connection():
+    # t = "curl --request GET \
+    # 	--url https://developer-api.moises.ai/api/application \
+    # 	--header 'Authorization: {}'".format(api_key)
+
+    url = 'https://developer-api.moises.ai/api/application'
+
+    headers = {
+        'Authorization': api_key
+    }
+    response = requests.get(url, headers=headers)
+    print(response)
+    
+def request_upload_download_url():
+    # curl --request GET \
+	# --url https://developer-api.moises.ai/api/upload \
+	# --header 'Authorization: your-api-key-here'
+    url = "https://developer-api.moises.ai/api/upload"
+    
+    headers = {
+        'Authorization': api_key
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def upload_song(upload_url):
+    # curl --request PUT \
+	# --url https://storage.googleapis.com/upload/something \
+	# --header 'content-type: multipart/form-data' \
+	# --form filedata=@./track.mp3
+    
+    headers = {'content-type': 'multipart/form-data'}
+
+    files={'filedata': ('John Mayer - New Light (Official Audio) [2PH7dK6SLC8].mp3', open('./John Mayer - New Light (Official Audio) [2PH7dK6SLC8].mp3', 'rb'))}
+    
+    response = requests.put(upload_url, headers=headers, files=files)
+    return response
+
+def download_file(download_url):
+    # curl --request POST \
+    #     --url https://developer-api.moises.ai/api/job \
+    #     --header 'Authorization: your-api-key-here' \
+    #     --header 'Content-Type: application/json' \
+    #     --data '{
+    # "name": "My job 123",
+    # "workflow": "my-workflow-id",
+    # "params": {
+    #     "inputUrl": "https://storage.googleapis.com/download/something"
+    # }
+    # }'
+    
+    url = "https://developer-api.moises.ai/api/job"
+
+    headers = {
+        'Authorization': api_key,
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "name": "My Testing Request",
+        "workflow": "000",
+        "params": {
+            "inputUrl": download_url
+        }
+    }
+    return requests.post(url, headers=headers, json=data)
+
+
+if __name__ == "__main__":
+    # res = request_upload_download_url()
+    # upload_url = res["uploadUrl"]
+    # download_url= res["downloadUrl"]
+    
+    # res_upload = upload_song(upload_url)
+    # print(res_upload)
+    # print(res_upload.text)
+    
+    # res_download = download_file(download_url)
+    # print(res_download)
+    # print(res_download.text)
+
+    pass
--- helper.py
+++ helper.py
@@ -1,30 +1,80 @@
 import tkinter as tk
 import os
+import subprocess
 
-
+#TODO if file exists we have a problem, works if file not downloaded
 
 class Interface:
     def __init__(self):
         root = tk.Tk()
         root.title("Antons Music Man")
 
+        #Textfield to copy the link into
         self.text_input = tk.Entry(root)
         self.text_input.pack()
 
-        submit_button = tk.Button(root, text="Submit", command=self.process_input)
+        #Button to start the download
+        submit_button = tk.Button(root, text="Submit", command=self.__process_input)
         submit_button.pack()
 
         root.mainloop()
 
-    
-    def process_input(self):
+    #Executed if the button is clicked
+    #
+    def __process_input(self):
+        #downloads the youtube video as mp3 
         url = self.text_input.get()
-        #TODO sent URL to downloader
         print("Anton wants to download: {}".format(url))
-        os.system(self.get_command(url))
+        output = subprocess.check_output(self.__get_command(url), shell=True, text=True)
+        #print(output)
+        
+        #find the name of the downloaded file
+        filename = None
+        for line in output.split("\n"):
+            if line.startswith("[ExtractAudio]"):
+                filename = self.__filename(line)
+                print("===> "+ filename + "<===")
+        
+        #if the filename was found, open the finder
+        if filename is not None:
+            self.__show_in_finder(filename)
+        else:
+            print("Filename not found in finder")
+
+    #finds the filename in a given string
+    def __filename(self, line):
+        x = line.split()
+
+        file_exists = None
+        if line.find("[ExtractAudio] Not converting audio") != -1:
+            print("*** FILE ALREADY DOWNLOADED ***")
+            file_exists = True
+        elif line.find("[ExtractAudio] Destination: ") != -1:
+            print("*** FILE NOT DOWNLOADED YET ***")
+            file_exists = False
+        
+        start = 4 if file_exists else 2
+        end = None
+        
+        for i in range(0,len(x)):
+            if x[i].find(".mp3") != -1:
+                end = i+1
+                break
+            i += 1
+
+        result_string = ""
+        for i in x[start:end]:
+            result_string += i+" "
+            
+        return result_string[:-1] if not file_exists else result_string[:-2]
 
     
-    def get_command(self, url):
-        print("DOWNLOADING")
+    def __get_command(self, url):
         return "yt-dlp -x --audio-format mp3 --audio-quality 0 {}".format(url)
 
+    def __show_in_finder(self,path):
+        subprocess.call(["open", "-R", path])
+
+
+if __name__ == "__main__":
+    Interface()
